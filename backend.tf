0e0db354eb23f6c6cab24f47f7fe8ad20b3a69b6
Daniel Gamboa <danielgamboa32@gmail.com>
2026-01-13 02:42:56 -0600

    Terraform format test success

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-github-actions-state-dangamboa"
-    key     = "infra/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
+    bucket       = "terraform-github-actions-state-dangamboa"
+    key          = "infra/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
     use_lockfile = true
   }
 }
--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket" "test_bucket" {
   bucket = "terraform-github-actions-dangamboa-test"
   tags = {
     Name = "ManagedByTerraform"
-    }
+  }
 }
 
 #Output bucket name
