eb8bbe4983c15a454d35a9f70ba48778efa04d02
Daniel Gamboa <danielgamboa32@gmail.com>
2026-01-13 02:25:44 -0600

    Initial terraform test

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket  = "terraform-github-actions-state-dangamboa"
+    key     = "infra/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+    use_lockfile = true
+  }
+}
---
+++ main.tf
@@ -0,0 +1,19 @@
+#Simple test
+
+# Configure the AWS provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create an S3 bucket
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "terraform-github-actions-dangamboa-test"
+  tags = {
+    Name = "ManagedByTerraform"
+    }
+}
+
+#Output bucket name
+output "bucket_name" {
+  value = aws_s3_bucket.test_bucket.bucket
+}
